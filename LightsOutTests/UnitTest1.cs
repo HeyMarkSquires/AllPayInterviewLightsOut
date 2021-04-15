72840d5c5032940bb5a6518d7749eb00636db5ae
Mark Squires <mark.squires@energisetechnology.co.uk>
2021-04-15 20:50:19 +0100

    Added some tests

--- LightsOutTests/UnitTest1.cs
+++ LightsOutTests/UnitTest1.cs
@@ -1,18 +1,94 @@
+using AllPayInterviewLightsOut;
 using NUnit.Framework;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace LightsOutTests
 {
     public class Tests
     {
+        private Form1 f;
         [SetUp]
         public void Setup()
         {
+            f = new Form1();
         }
 
+        /// <summary>
+        /// A test to make sure that a grid of green buttons is not counted as a win state
+        /// </summary>
         [Test]
-        public void Test1()
+        public void GameIsNotInWinStateAllGreenButtons()
         {
-            Assert.Pass();
+            List<Button> l = new List<Button>();
+            for (var i = 0; i < 25; i++)
+            {
+                Button b = new Button();
+                b.BackColor = Color.Green;
+                l.Add(b);
+            }
+            Assert.AreEqual(f.checkWinState(l), false);
+        }
+
+        /// <summary>
+        /// A test to make sure that a grid of white buttons, but one green button, is not counted as a win state
+        /// </summary>
+        [Test]
+        public void GameIsNotInWinStateOneGreenButton()
+        {
+            List<Button> l = new List<Button>();
+            for (var i = 0; i < 25; i++)
+            {
+                Button b = new Button();
+                b.BackColor = Color.White;
+                l.Add(b);
+            }
+            l[13].BackColor = Color.Green;
+            Assert.AreEqual(f.checkWinState(l), false);
+        }
+
+
+        /// <summary>
+        /// A test to make sure that a grid of white buttons is counted as a win state
+        /// </summary>
+        [Test]
+        public void GameIsInWinState()
+        {
+            List<Button> l = new List<Button>();
+            for (var i = 0; i < 25; i++)
+            {
+                Button b = new Button();
+                b.BackColor = Color.White;
+                l.Add(b);
+            }
+            Assert.AreEqual(f.checkWinState(l), true);
+        }
+
+
+        /// <summary>
+        /// Test to make sure that a button that is green is successfully toggled to white
+        /// </summary>
+        [Test]
+        public void ButtonToggleToWhiteTest()
+        {
+            Button myButton = new Button();
+            myButton.BackColor = Color.Green;
+            f.toggleColour(myButton);
+            Assert.AreEqual(myButton.BackColor, Color.White);
+        }
+
+        /// <summary>
+        /// Test to make sure that a button that is white is successfully toggled to green
+        /// </summary>
+        [Test]
+        public void ButtonToggleToGreenTest()
+        {
+            Button myButton = new Button();
+            myButton.BackColor = Color.White;
+            f.toggleColour(myButton);
+
+            Assert.AreEqual(myButton.BackColor, Color.Green);
         }
     }
 }
